c6105b5e3c2cb53b11353d74a3c8485bc38f1946
mzsv <andremzsv@gmail.com>
2023-12-05 16:12:30 +0000

    day_05_2_ongoing

--- day_05/ex02.py
+++ day_05/ex02.py
@@ -1,15 +1,16 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    ex01.py                                            :+:      :+:    :+:    #
+#    ex02.py                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: amenses- <amenses-@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/12/05 13:16:04 by amenses-          #+#    #+#              #
-#    Updated: 2023/12/05 15:24:01 by amenses-         ###   ########.fr        #
+#    Created: 2023/12/05 15:25:47 by amenses-          #+#    #+#              #
+#    Updated: 2023/12/05 16:10:49 by amenses-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+offset = 169908
 almanac = []
 with open("input.txt", "r") as f:
     seeds = f.readline().split(":")[1].split()
@@ -28,6 +29,8 @@ def search_location(value, index):
         index += 1
         if (len(row) < 3):
             return (search_location(value, index + 1))
+        if (next == 1):
+            continue
         row = [int(x) for x in row]
         if value != map_value(value, range(row[1], row[1] + row[2]), range(row[0], row[0] + row[2])) and next == 0:
             value = map_value(value, range(row[1], row[1] + row[2]), range(row[0], row[0] + row[2]))
@@ -35,6 +38,16 @@ def search_location(value, index):
     return (value)
 
 # result
-print(min([search_location(int(seed), 0) for seed in seeds]))
+seeds = [int(x) for x in seeds]
+r1 = range(seeds[0], seeds[0] + seeds[0 + 1])
+m = search_location(seeds[0], 0)
+for seed in range(seeds[0], seeds[0] + seeds[0 + 1]):
+    print(search_location(seed, 0))
 
-# SOLUTION: 340994526
+# print(min([search_location(seed, 0) for seed in seeds]))
+# results = []
+# for i in range(0, len(seeds), 2):
+#     # results.append(min([search_location(seed, 0) for seed in range(seeds[i], seeds[i] + seeds[i + 1])]))
+# print("result:", min(results))
+
+# SOLUTION:
